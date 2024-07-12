e7ae3348d22a273f4f507ad3c27cbe0f0484b05f
Yihun <yankure01@gmail.com>
2024-07-13 01:42:48 +0300

    post_repo

--- lib/features/post/repo/post_repo.dart
+++ lib/features/post/repo/post_repo.dart
@@ -1,3 +1,53 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:project_2/features/post/model/post.dart';
+import 'package:uuid/uuid.dart';
+
 class PostRepo {
-  
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _cloud = FirebaseFirestore.instance;
+  final FirebaseStorage _storage = FirebaseStorage.instance;
+
+  Future<void> createImagePost({required Post post, required List<String> imageUrls}) async {
+    try {
+      final postId = await _cloud.collection('posts').add(
+        {
+          'id': '',
+          'type': post.type,
+          'content': post.content,
+          'images': imageUrls,
+          'likes': 0,
+          'saves': 0,
+          'shares': 0,
+          'commentIDs': [],
+          'createdAt': DateTime.now().toString().substring(0, 10),
+          'updatedAt': DateTime.now().toString().substring(0, 10),
+          'userId': _auth.currentUser!.uid,
+          'place': null,
+        },
+      );
+      await _cloud.collection('posts').doc(postId.id).update({'id': postId.id});
+    } catch (e) {
+      print("An Error has occured while creating image post $e");
+    }
+  }
+
+  // Add Image
+  Future<String> addImage(File image) async {
+    try {
+      final uid = _auth.currentUser!.uid;
+      final String randomImageName = '${const Uuid().v4()}.jpg';
+      final storageRef = _storage.ref().child('$uid/posts/$randomImageName');
+      UploadTask uploadTask = storageRef.putFile(image);
+      TaskSnapshot taskSnapshot = await uploadTask.whenComplete(() => null);
+      String imageUrl = await taskSnapshot.ref.getDownloadURL();
+      return imageUrl;
+    } catch (e) {
+      print('Error while adding image ${e.toString()}');
+      return '';
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:project_2/features/todo/models/task_model.dart';
 import 'package:project_2/features/todo/screens/home_screen.dart';
 import 'package:project_2/features/todo/task_bloc/task_bloc.dart';
+import 'package:project_2/firebase_options.dart';
 
-void main() {
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
